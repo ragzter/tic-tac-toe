5c2d93a79097569fde856eb499ab181e5d959f93
Ragnar Hedenskog <ragnar.hedenskog@gmail.com>
2018-09-21 21:14:55 +0200

    Initial commit

---
+++ TTT.hs
@@ -0,0 +1,42 @@
+
+-- Data types
+
+data Square = Blank | Zero | Cross
+  deriving (Eq)
+
+type Row = [Square]
+type Board = [Row]
+
+-- Functions for creating rows and boards
+
+createRow :: Row
+createRow = [Zero, Zero, Zero]
+
+createBoard :: Board
+createBoard = [createRow, createRow, createRow]
+
+-- Functions for printing a Square / Row / Board
+
+printSquare :: Square -> IO ()
+printSquare Blank = putStr " "
+printSquare Zero =  putStr "0"
+printSquare Cross = putStr "x"
+
+printRow :: Row -> IO ()
+printRow (x:[]) = do
+  printSquare x
+  putStr "\n"
+printRow (x:xs) = do
+  printSquare x
+  putStr " "
+  printRow xs
+
+printBoard :: Board -> IO ()
+printBoard (x:[]) = printRow x
+printBoard (x:xs) = do
+  printRow x
+  printBoard xs
+
+-- Functions for updating a game
+
+-- put :: Board -> Board
