a0069e3d9bbbed7052d7fa1f1218e14f5ed60748
Ragnar Hedenskog <ragnar.hedenskog@gmail.com>
2018-09-22 16:05:40 +0200

    Made `Square` `Show` compliant

--- TTT.hs
+++ TTT.hs
@@ -7,6 +7,11 @@ import System.IO
 data Square = Blank | Zero | Cross
   deriving (Eq)
 
+instance Show Square where
+  show Cross = "x"
+  show Zero = "0"
+  show Blank = " "
+
 opponentOf :: Square -> Square
 opponentOf Zero = Cross
 opponentOf Cross = Zero
